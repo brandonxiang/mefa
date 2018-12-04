df377198424da625ddc8ae25f4cd88d3cbb324dd
brandonxiang <1542453460@qq.com>
2018-12-04 22:22:22 +0800

    fix: react 路由更新报错的情况

--- examples/sub-react/index.js
+++ examples/sub-react/index.js
@@ -3,12 +3,25 @@ import { HashRouter as Router, Route, Link, withRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import Mefa from '../../dist/mefa.es';
 
-const Control = withRouter(({ history }) => {
-  Mefa.onRouteUpdate((route) => {
-    history.push(route);
-  });
-  return <span>Powered by React</span>;
-})
+
+class Control extends Component {
+  constructor() {
+    super()
+  }
+
+  componentDidMount() {
+    Mefa.onRouteUpdate((route) => {
+      this.props.history.push(route);
+    });
+  }
+
+  render() {
+    return <span>Powered by React</span>;
+  }
+}
+
+const ControlWithRouter = withRouter(Control);
+
 const PageOne = () => <h1>System2 Page1</h1>;
 const PageTwo = () => <h1>System2 Page2</h1>;
 const PageThree = () => <h1>System2 Page3</h1>;
@@ -20,7 +33,7 @@ class App extends Component {
       <div>
         <Router>
           <div>
-            <Control></Control>
+            <ControlWithRouter></ControlWithRouter>
             <Route path="/" component={PageOne} exact={true}></Route>
             <Route path="/1" component={PageTwo}></Route>
             <Route path="/2" component={PageThree}></Route>
